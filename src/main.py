7707cf678b17b4c4f660b4021a175b0798ca200c
Vyrewatch <sirvyrewatch@hotmail.com>
2023-08-02 14:54:20 -0300

    Organizado melhor o MVC

--- src/main.py
+++ src/main.py
@@ -1,23 +1,5 @@
-import discord
-from discord.ext import commands
-from decouple import config
 from controllers import control
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(intents=intents,command_prefix='!')
-
-@bot.command(name='rank')
-async def rank(ctx,*,arg):
-    await ctx.send(control.rank_check(arg))
-
-@bot.command(name='rsge')
-async def rsge(ctx,*,arg):
-    await ctx.send(control.ge_pricecheck(arg))
-
-@bot.command(name='cot')
-async def cot(ctx,*,arg):
-    await ctx.send(control.cot(arg))
-
-bot.run(config('discord_bot_key'))
+if __name__ == "__main__":
+    starter = control.Discord_bot
+    starter.run()
--- src/models/model.py
+++ src/models/model.py
@@ -2,84 +2,90 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup as soup
 
-from controllers.control import MyClient
+class Commands_methods:
+    def cot(argument):
+        argument = argument.upper()
+        if ' ' in argument:
+            tocon = argument.split(' ')[0]
+            fromcon = argument.split(' ')[1]
+            cota = requests.get(
+                f'https://economia.awesomeapi.com.br/json/last/{tocon}-{fromcon}'
+            )
+            cota = cota.json()
+            output = cota[f'{tocon}{fromcon}']['bid']
+            return f'The conversion {tocon} to {fromcon} is: {output}'
+        
+        try:
+            cota = requests.get(
+                    f'https://economia.awesomeapi.com.br/json/last/{argument}'
+                )
+            cota = cota.json()
+            output = cota[f'{argument}BRL']['bid']
+            return f'The conversion {argument} to BRL is: {output}'
+        except:
+            return f'Sorry but you commited an input error (Ooopsie)\neither that, or the API server is offline.'
 
 
-async def cot(argument):
-    argument = argument.upper()
-    if ' ' in argument:
-        argument = argument.replace(' ', '-')
 
-    cota = requests.get(
-        f'https://economia.awesomeapi.com.br/json/last/{argument}'
-    )
-    cota = cota.json()
-    try:
-        output = cota[f'{argument}']['bid']
-        await MyClient.message.channel.send(
-            f'The conversion {argument} is: {output}'
+    def rank_check(argument):
+        osrsrankresponse = requests.get(
+            f'https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player={argument}'
         )
-    except:
-        await MyClient.message.channel.send('Currency not found.')
+        skills = [
+            'Overall',
+            'Attack',
+            'Defence',
+            'Strength',
+            'Hitpoints',
+            'Ranged',
+            'Prayer',
+            'Magic',
+            'Cooking',
+            'Woodcutting',
+            'Fletching',
+            'Fishing',
+            'Firemaking',
+            'Crafting',
+            'Smithing',
+            'Mining',
+            'Herblore',
+            'Agility',
+            'Thieving',
+            'Slayer',
+            'Farming',
+            'Runecrafting',
+            'Hunter',
+            'Construction',
+        ]
+        levels = []
+        ranks = osrsrankresponse.text
+        rows = ranks.split('\n')
+        for i, row in enumerate(rows):
+            if i == 24:
+                break
+            column = row.split(',')
+            levels.append(column[1])
+        data = {'Skills': skills, 'Level': levels}
+        df = pd.DataFrame(data)
+        return df
 
 
-async def rank_check(argument):
-    osrsrankresponse = requests.get(
-        f'https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player={argument}'
-    )
-    skills = [
-        'Overall',
-        'Attack',
-        'Defence',
-        'Strength',
-        'Hitpoints',
-        'Ranged',
-        'Prayer',
-        'Magic',
-        'Cooking',
-        'Woodcutting',
-        'Fletching',
-        'Fishing',
-        'Firemaking',
-        'Crafting',
-        'Smithing',
-        'Mining',
-        'Herblore',
-        'Agility',
-        'Thieving',
-        'Slayer',
-        'Farming',
-        'Runecrafting',
-        'Hunter',
-        'Construction',
-    ]
-    levels = []
-    ranks = osrsrankresponse.text
-    rows = ranks.split('\n')
-    for i, row in enumerate(rows):
-        if i == 24:
-            break
-        column = row.split(',')
-        levels.append(column[1])
-    data = {'Skills': skills, 'Level': levels}
-    df = pd.DataFrame(data)
-    await MyClient.message.channel.send(df)
+    def ge_pricecheck(argument):
+        try:
+            url = f'https://oldschool.runescape.wiki/w/{argument}'
+            html = requests.get(url)
+            bs = soup(html.text, 'html.parser').find('div', class_='GEdataprices')
+            item_id = bs['data-itemid']
+        except:
+            return "Couldn't find the item."
 
+        osrsgeresponse = requests.get(
+            f'https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item={item_id}'
+        )
+        data = osrsgeresponse.json()
+        dataname = data['item']['name']
+        datadesc = data['item']['description']
+        dataprice = data['item']['current']['price']
+        datatoday = data['item']['today']['price']
 
-async def ge_pricecheck(argument):
-    url = f'https://oldschool.runescape.wiki/w/{argument}'
-    html = requests.get(url)
-    bs = soup(html.text, 'html.parser').find('div', class_='GEdataprices')
-    item_id = bs['data-itemid']
-
-    osrsgeresponse = requests.get(
-        f'https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item={item_id}'
-    )
-    data = osrsgeresponse.json()
-    dataname = data['item']['name']
-    datadesc = data['item']['description']
-    dataprice = data['item']['current']['price']
-    datatoday = data['item']['today']['price']
-    await MyClient.message.channel.send(
-        f"Name: {dataname}\nDescription: {datadesc}\nPrice: {dataprice}\nToday's change: {datatoday}"
-    )
+        return f"Name: {dataname}\nDescription: {datadesc}\nPrice: {dataprice}\nToday's change: {datatoday}"
